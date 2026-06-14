c8856b31b52f0a629895dd3e8fa5563ddbfe515b
noname9921 <baobao1102@gmail.com>
2026-06-14 18:26:03 +0700

    Updates

--- assets/Prerequisite-CPpop2b6.js
+++ assets/Prerequisite-CPpop2b6.js
@@ -1,4 +1,4 @@
-import{n as e,r as t}from"./index-CM33Qdcu.js";import{t as n}from"./SwirlBackground-CbQbPc93.js";import{t as r}from"./lib-0jxv0p6p.js";var i=`# Prerequisites\r
+import{n as e,r as t}from"./index-JBMgArVU.js";import{t as n}from"./SwirlBackground-BRfOSpjH.js";import{t as r}from"./lib-DzbIooSr.js";var i=`# Prerequisites\r
 \r
 This platform presupposes a foundational level of mathematical literacy on the part of the User. Prior to engaging with any material presented herein, Users are strongly advised to possess a confident and working knowledge of the following competencies:\r
 \r
